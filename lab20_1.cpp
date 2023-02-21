f0e68918c68dfb4b1e623683d29e346fc9419d0f
Pannawit Setsiriwanit <pannawit_se@cmu.ac.th>
2023-02-21 18:56:50 +0700

    finishes lab20

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,7 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+    if (min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x)<0||min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h)<0) return 0;
+    return (min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x))*(min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h));
 }
